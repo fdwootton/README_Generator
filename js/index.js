45067fd3f912bae7941c391748c54b23d1aff88a
Felicia Wootton <felicia.wootton@gmail.com>
2021-03-30 18:54:19 -0500

    Added prompt questions

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const prompts = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Enter a description of your project:',
+        name: 'description'
+    }
+]
