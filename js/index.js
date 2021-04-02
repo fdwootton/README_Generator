d50edf22a19002b9d5187659cc3b2c09e5b3cd11
Felicia Wootton <felicia.wootton@gmail.com>
2021-04-02 09:12:14 -0500

    Added htmlgenerator function

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const prompts = [
+const questions = [
     {
         type: 'input',
         message: 'What is the title of your project?',
@@ -13,3 +13,42 @@ const prompts = [
         name: 'description'
     }
 ]
+
+const generateHTML = (response) =>
+  `<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>${response.title}</title>
+    </head>
+    <body>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">${response.title}</h1>
+                <p class="lead">${response.description}.</p>
+                <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+                <ul class="list-group">
+                <li class="list-group-item">My GitHub username is ${answers.github}</li>
+                <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>`;
+
+
+inquirer
+
+    .prompt(questions)
+
+    .then((response) => {
+        
+        const HTML = generateHTML(response);
+
+        fs.writeFile('index.html', htmlPageContent, (err) =>
+          err ? console.log(err) : console.log('Successfully created index.html!')
+        );
+    
+    })
