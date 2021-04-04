2ab875cee1e1633576d831358c31d83bd93eec60
Felicia Wootton <felicia.wootton@gmail.com>
2021-04-04 13:49:50 -0500

    Added questions and markdown

--- js/index.js
+++ js/index.js
@@ -11,41 +11,97 @@ const questions = [
         type: 'input',
         message: 'Enter a description of your project:',
         name: 'description'
+    },
+    {
+        type: "input",
+        message: "Enter the installation process for your project: ",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "What is your project used for?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "List the contributors of this projects:",
+        name: "contributors"
+    },
+    {
+        type: "input",
+        message: "Enter any test instructions:",
+        name: "tests"
+    },
+    {
+        type: "list",
+        message: "Choose a license for your project: ",
+        choices: [
+            "Apache",
+            "GNU GPLv3",
+            "MIT",
+            "ISC"
+        ],
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "Enter your GitHub username:",
+        name: "username",
+    },
+    {
+        type: "input",
+        message: "Enter your email address:",
+        name: "email"
     }
 ]
 
-const generateHTML = (response) =>
-  `<!DOCTYPE html>
-<html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <title>${response.title}</title>
-    </head>
-    <body>
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">${response.title}</h1>
-                <p class="lead">${response.description}.</p>
-                <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-                <ul class="list-group">
-                <li class="list-group-item"></li>
-                <li class="list-group-item"></li>
-                </ul>
-            </div>
-        </div>
-    </body>
-</html>`;
+const generateREADME = (response) =>
+  `
+  <h1 align="center">${response.title}</h1>
+  
+  ![badge](https://img.shields.io/badge/license-${response.license}-blue)<br />
+  ## Description
+  ${response.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributors](#contributors)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${response.installation}
+
+  ## Usage
+  ${response.usage}
+
+  ## License
+  ![badge](https://img.shields.io/badge/license-${response.license}-brightgreen)
+  <br />
+
+  ${response.license} license
+
+  ## Contributors
+  ${response.contributors}
+
+  ## Tests
+  ${response.tests}
+
+  ## Questions
+  For additional questions, email me at ${response.email}
+  GitHub profile: [${response.username}](https://github.com/${response.username})
+  `;
 
 
-inquirer
+  inquirer
 
     .prompt(questions)
 
     .then((response) => {
         
-        const HTML = generateHTML(response);
+        const HTML = generateREADME(response);
 
         fs.writeFile('README.md', HTML, (error) =>
           error ? console.log(error) : console.log('New Readme created!')
